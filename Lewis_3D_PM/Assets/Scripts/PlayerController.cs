be75d8416b22c1ae92fbad134d7ffc97b1180963
Ian Lewis <33504743+wellthathappened@users.noreply.github.com>
2026-09-11 13:43:21 -0700

    Setup script for movement (not finished)

--- Lewis_3D_PM/Assets/Scripts/PlayerController.cs
+++ Lewis_3D_PM/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 5.0f;
+    public float jumpHeight = 10.0f;
+
+    public Vector2 moveInput = Vector2.zero;
+
+    PlayerInput input;
+    Rigidbody rb;
+    
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        input = GetComponent<PlayerInput>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
